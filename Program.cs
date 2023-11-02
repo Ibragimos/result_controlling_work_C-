8d5418fb55bba381f3fc877d881c1f81ab7e585e
Marina <korhovama@mail.ru>
2023-11-02 08:25:38 +0300

    Add ArrElements

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@ int ArrRandomSize()
 {
     return random.Next(0, 4);
 }
-
+void ArrElements(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Console.ReadLine()!;
+    }
+}
 
 
 
