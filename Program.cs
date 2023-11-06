1dcf68271f1022d49b41261e279e5f5526fe3aa3
Marina <korhovama@mail.ru>
2023-11-06 17:49:40 +0300

    Add method  ReducedArray

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ Random random = new Random();
 
 int ArrRandomSize()
 {
-    return random.Next(3, 6);
+    return random.Next(4, 7);
 }
 
 void ArrRandomElements(string[] arr)
@@ -21,7 +21,25 @@ string EndingString(int size)
     else return "строк";
 }
 
+void ReducedArray(string[] arr)
+{
+    string[] newArr = new string[random.Next(0, 4)];
+    int sizeNewArr = newArr.Length;
+    System.Console.Write($"Вывод {sizeNewArr} строк: ");
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        
+        string temp = arr[i];
+        arr[i] = newArr[i];
+        newArr[i] = temp;
+        System.Console.Write($"[{newArr[i]}] ");
+    }
+}
+
 int size = ArrRandomSize();
 string[] arr = new string[size];
 System.Console.WriteLine($"Введите {size} {EndingString(size)} через Enter");
 ArrRandomElements(arr);
+ReducedArray(arr);
+
+
